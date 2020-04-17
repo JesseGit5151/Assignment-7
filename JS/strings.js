64a92188676ea9af7dc36b97a2a44ef10e1fc728
JesseGit5151 <jrodriguez5151yahoo.com>
2020-04-16 22:32:11 -0700

    fe

---
+++ JS/strings.js
@@ -0,0 +1,29 @@
+/*eslint-env browser*/
+
+//STEP 1
+// let name = prompt("Please enter your name");
+// console.log(name.length);
+//STEP 2
+// let name = prompt("Please enter your name");
+// let num = prompt("Please enter a number.");
+//?
+//STEP 3
+// let name = prompt("Please enter your name");
+// let lastName = prompt("Please enter your last name.");
+// let fullName = name.concat(lastName);
+// alert("Youre name is: " + fullName);
+//STEP 4
+// let text = "The quick brown fox jumps over the lazy dog";
+// console.log(text.indexOf("fox"));
+//STEP 5
+let text = "The quick brown fox jumps over the lazy fox";
+console.log(text.lastIndexOf("fox"));
+//STEP 6
+
+//STEP 7
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
