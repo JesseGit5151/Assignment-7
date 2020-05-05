04c31bc6b03d03f08e029355421489ddd3a1cd60
JesseGit5151 <jrodriguez5151yahoo.com>
2020-05-05 12:18:07 -0700

    sc

--- JS/math.js
+++ JS/math.js
@@ -1,11 +1,25 @@
 /*eslint-env browser*/
 
 //STEP 1
-
+// let numPrompt = prompt('Please enter a number.');
+// console.log(Math.abs(numPrompt));
 //STEP 2
-
+// let numPrompt = prompt('Please enter a number with a decimal.');
+// console.log(Math.ceil(numPrompt));
 //STEP 3
-
+// let numPrompt = prompt('Please enter a number with a decimal.');
+// console.log(Math.floor(numPrompt));
 //STEP 4
-
+// let numPrompt;
+// let arr = [];
+// let i = 0;
+// while(i < 5) {
+//     numPrompt = parseInt(prompt('Please enter a number.'));
+//     arr.push(numPrompt)
+//     i++;
+// }
+// console.log(arr);
+// console.log(Math.max(...arr));
 //STEP 5
+// let numPrompt = prompt('Please enter a number.');
+// console.log(Math.pow(numPrompt, numPrompt));
--- JS/strings.js
+++ JS/strings.js
@@ -16,14 +16,24 @@
 // let text = "The quick brown fox jumps over the lazy dog";
 // console.log(text.indexOf("fox"));
 //STEP 5
-let text = "The quick brown fox jumps over the lazy fox";
-console.log(text.lastIndexOf("fox"));
+// let text = "The quick brown fox jumps over the lazy fox";
+// console.log(text.lastIndexOf("fox"));
 //STEP 6
-
+// let text = "The quick brown fox jumps over the lazy dog";
+// let fullName = prompt("Please enter your full name");
+// alert(text.replace("the lazy dog", fullName));
 //STEP 7
-
+// let text = "The quick brown fox jumps over the lazy dog";
+// let word = prompt("Please enter a word");
+// alert(text.search(word));
 //STEP 8
-
+// let old_string = "The quick brown fox jumps over the lazy dog";
+// let new_string = old_string.slice(31).toUpperCase();
+// alert(new_string)
 //STEP 9
-
+// let text =         "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          ";
+// alert(text.trim().toLowerCase());
 //STEP 10
+// let text = "The quick brown fox jumps over the lazy dog";
+// text.charAt(0).toUpperCase();
+// console.log(text);
